597f1374ffff639017bc6e1cfdac90a7b5304d71
Tejashree Sayankar <tejashreesayankar@gmail.com>
2026-03-30 18:01:45 +0530

    Updated constants.py

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -7,5 +7,10 @@ MESSAGES = {
     "USER_ALREADY_EXISTS": "User with this email already exists.",
     "USER_NOT_FOUND": "User not found.",
     "INVALID_CREDENTIALS": "Invalid email or password.",
+    "PASSWORD_RESET": "Password reset successfully.",
+    "OTP_SENT": "OTP sent on registered email, please check.",
+    "INVALID_OTP": "Please enter correct OTP.",
+    "LOGIN_SUCCESS": "User logged in successfully.",
+    "PASSWORD_CHANGED": "Password changed successfully."
     
 }
